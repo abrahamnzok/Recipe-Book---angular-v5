f09535d0187dec76d5a513f7c4e5a0c1d69a664c
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-01-28 11:40:07 +0100

    removed recipes instantiation.

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {Recipes} from './recipes.model';
 
 @Component({
   selector: 'app-recipes',
@@ -8,12 +7,8 @@ import {Recipes} from './recipes.model';
 })
 export class RecipesComponent implements OnInit {
 
-  recipes: Recipes[] = [
-    new Recipes('Rands Fried Chicken', 'Your recipe description goes here', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
-    new Recipes('Mom\'s World Famous Banana Bread', 'This classic banana bread recipe comes from my mom -- the walnuts add a nice texture and flavor to the banana bread', 'http://assets.blog.foodnetwork.ca/imageserve/wp-content/uploads/sites/6/2016/11/chocolate-banana-pancake-breakfast-casserole/x.jpg')
-  ];
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
