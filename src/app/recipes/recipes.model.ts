893cd7bdef0dae82163f184412f03a95241a3744
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-01-28 11:39:08 +0100

    removed unnecessary getters and setters methods.

--- src/app/recipes/recipes.model.ts
+++ src/app/recipes/recipes.model.ts
@@ -1,35 +1,14 @@
 
 export class Recipes {
 
-  private _name: string;
-  private _description: string;
-  private _src: string;
+  public name: string;
+  public description: string;
+  public src: string;
 
   constructor(name: string, description: string, src: string) {
-    this._name = name;
-    this._description = description;
-    this._src = src;
+    this.name = name;
+    this.description = description;
+    this.src = src;
   }
 
-  getName(): string {
-    return this._name;
-  }
-
-  setName(value: string) {
-    this._name = value;
-  }
-  getSrc(): string {
-    return this._src;
-  }
-
-  setSrc(value: string) {
-    this._src = value;
-  }
-  getDescription(): string {
-    return this._description;
-  }
-
-  setDescription(value: string) {
-    this._description = value;
-  }
 }
